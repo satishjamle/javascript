21ca56d4720fd6a0f039ae621dafca2cec5cfacf
Satish Jamle <138292018+satishjamle@users.noreply.github.com>
2024-02-12 15:40:14 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,17 @@
+// console.log("welcome bro")
+// function getA(){
+//     getB();
+//     document.write("get a valled");
+
+// }
+//     function getB(){
+//         getC();
+//         document.write("getb called b");
+//     }
+//     function getC(){
+//         getA();
+//         document.write("get c called")
+//     }
+
+// getA();
+   
---
+++ index.js
@@ -0,0 +1,42 @@
+// console.log("hello user")
+// prompt("this is a javascript
+// let a = 10;
+// let b = 20;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+
+// function sum(a,b,c){
+
+    // document.write(a+b +"<br>");
+    // document.write(a*b +"<br>");
+    // document.write(a-b +"<br>");
+//     var a = a+b+c;
+//     return a;
+// }
+// var total =  sum(2,3,5);
+// document.write(total)
+// // function hello(){
+//     document.write(" this is mu function a javascript")
+
+   
+   
+//     document.write(" this is mu function a javascript")
+//     document.write("<br>")
+
+//     document.write(" this is mu function a javascript")
+
+// }
+// hello();
+// document.write("<br>")
+// document.write(" this is mu function a javascript")
+
+// function hello(fname,lname) {
+//     // document.write(`${fname}${lname}`)
+//    document.write(fname +""+ lname + "<br>");
+    
+// }
+
+// hello("ajay","solanki");
+// hello("vijay","solanki");
+// hello("sumit","solanki");
